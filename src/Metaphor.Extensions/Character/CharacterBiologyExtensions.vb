151bda39921b51de07a109523691f1c6fe036c98
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-09-05 07:08:33 -0500

    full bowel causes damage

--- src/Metaphor.Extensions/Character/CharacterBiologyExtensions.vb
+++ src/Metaphor.Extensions/Character/CharacterBiologyExtensions.vb
@@ -8,10 +8,16 @@ Friend Module CharacterBiologyExtensions
             Return
         End If
         Dim stomach = Math.Min(amount, character.GetStomach())
+        Dim damage = 0
         If stomach > 0 Then
             amount -= stomach
             character.DoChangeCounter(Counters.STOMACH, -stomach)
-            character.DoChangeCounter(Counters.BOWEL, stomach)
+            Dim bowel = Math.Min(stomach, character.GetCounterCapacity(Counters.BOWEL))
+            damage += (stomach - bowel)
+            If damage > 0 Then
+                character.AddMessage($"{character.Name} takes damage from having a full bowel!")
+            End If
+            character.DoChangeCounter(Counters.BOWEL, bowel)
         End If
         Dim satiety = Math.Min(amount, character.GetSatiety())
         If satiety > 0 Then
@@ -19,15 +25,16 @@ Friend Module CharacterBiologyExtensions
             character.DoChangeCounter(Counters.SATIETY, -satiety)
         Else
             If character.IsCounterMaximum(Counters.SATIETY) Then
-                If Not character.IsCounterMaximum(Counters.HEALTH) Then
+                If Not character.IsCounterMaximum(Counters.HEALTH) AndAlso damage <= 0 Then
                     character.DoChangeCounter(Counters.HEALTH, 1)
                 End If
             Else
                 character.DoChangeCounter(Counters.SATIETY, 1)
             End If
         End If
-        If amount > 0 Then
-            character.DoChangeCounter(Counters.HEALTH, -amount)
+        damage += amount
+        If damage > 0 Then
+            character.DoChangeCounter(Counters.HEALTH, -damage)
         End If
     End Sub
 End Module
--- src/Metaphor.Extensions/Character/CharacterInitializationExtensions.vb
+++ src/Metaphor.Extensions/Character/CharacterInitializationExtensions.vb
@@ -15,8 +15,8 @@ Friend Module CharacterInitializationExtensions
         character.CreatePoopVerb()
         character.InitializeCounter(Counters.SATIETY, 100, 0, 100)
         character.InitializeCounter(Counters.HEALTH, 50, 0, 100)
-        character.InitializeCounter(Counters.STOMACH, 0, 0, 50)
-        character.InitializeCounter(Counters.BOWEL, 0, 0, 50)
+        character.InitializeCounter(Counters.STOMACH, 50, 0, 50)
+        character.InitializeCounter(Counters.BOWEL, 50, 0, 50)
     End Sub
 #End Region
 End Module
